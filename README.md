be043a4c6ff2ef2337da4698132ba9226fb2b875
towrig <karl32123@gmail.com>
2023-08-06 19:13:31 +0300

    add my own model to the scene

---
+++ README.md
@@ -0,0 +1,25 @@
+# Ideas
+
+- Particle effects for swords:
+    - flames attached to the weapon
+    - bloom
+    - emit particles
+- Particle effects for character
+    - show breath
+        - maybe even breathe fire?
+    - rings of smoke when punching a surface
+        - also shove a break texture on the area punched that dissapears over time
+    - learn OpenGL, maybe stuff can be done better
+        - investigate component shaders aswell...
+- See what is possible with bevy_rapier's advanced collision detection
+    - maybe punches can have nice flow and power?
+    - dismemberment
+    - can I reference bones based on the collision???
+
+
+# List of crates and other useful things
+
+- bevy - renderer
+- bevy_rapier - physics
+- bevy_hanabi - particles
+- gltf-viewer.donmccurdy.com - check if gltf files are correct
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .insert_resource(AmbientLight {
+        color: Color::WHITE,
+        brightness: 1.0 / 5.0f32,
+    })
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup,  setup)
+    .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
+    // plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 2.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(3.0, 1.0, 0.0),
+        ..default()
+    });
+    // man
+    commands.spawn(SceneBundle {
+        scene: asset_server.load("gltf/man_with_armature.gltf#Scene0"),
+        transform: Transform::from_xyz(0.0, 0.0, 0.0).with_scale(Vec3::new(0.1, 0.1, 0.1)),
+        ..default()
+    });
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 3500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
